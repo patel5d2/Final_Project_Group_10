52775a72491fbeb1a2b01582647a59a75a81d4c8
Shamak10 <patel8sd@mail.uc.edu>
2025-10-30 12:09:05 -0400

    Add About page to meet rubric requirements
    - Created comprehensive About page with project details
    - Added About action to HomeController
    - Added About link to navigation menu
    - Project now meets all rubric criteria including 6+ pages requirement

--- Final_Project_Group_10/Controllers/HomeController.cs
+++ Final_Project_Group_10/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Final_Project_Group_10.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
